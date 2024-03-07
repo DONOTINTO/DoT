e3e6933c9dd992f60d00c07602a27751b7069900
DONOTINTO <goods99j@gmail.com>
2024-03-07 14:30:48 +0900

    create. DoT proj

---
+++ DoT/DoT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoT
+//
+//  Created by 이중엽 on 3/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
