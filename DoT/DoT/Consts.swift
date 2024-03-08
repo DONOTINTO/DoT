8e9515d7c892f855c7cbe4eea46725e154bbfe58
DONOTINTO <goods99j@gmail.com>
2024-03-08 17:28:20 +0900

    feat. color set 추가 및 Consts로 정의

--- DoT/DoT/Consts.swift
+++ DoT/DoT/Consts.swift
@@ -5,7 +5,7 @@
 //  Created by 이중엽 on 3/8/24.
 //
 
-import Foundation
+import UIKit
 
 enum Consts {
     
@@ -15,5 +15,14 @@ enum Consts {
         case medium = 15
         case large = 19
         case extraLarge = 30
+    }
+    
+    enum Color {
+        
+        static let whiteBlack = UIColor(named: "WhiteBlack")
+        static let pointBlue = UIColor(named: "PointBlue")
+        static let justWhite = UIColor(named: "JustWhite")
+        static let justGray = UIColor(named: "JustGray")
+        static let justBlack = UIColor(named: "JustBlack")
     }
 }
