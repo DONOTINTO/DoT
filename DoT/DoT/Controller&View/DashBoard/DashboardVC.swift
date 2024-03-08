a96e6408607a83e461304601194e04f114251e1e
DONOTINTO <goods99j@gmail.com>
2024-03-08 17:52:26 +0900

    create. Dashboard VC/View 파일 추가

---
+++ DoT/DoT/Controller&View/DashBoard/DashboardVC.swift
@@ -0,0 +1,16 @@
+//
+//  DashboardViewController.swift
+//  DoT
+//
+//  Created by 이중엽 on 3/8/24.
+//
+
+import UIKit
+
+class DashboardViewController: BaseViewController<DashboardView> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ DoT/DoT/Controller&View/DashBoard/DashboardView.swift
@@ -0,0 +1,12 @@
+//
+//  DashboardView.swift
+//  DoT
+//
+//  Created by 이중엽 on 3/8/24.
+//
+
+import UIKit
+
+class DashboardView: BaseView {
+    
+}
