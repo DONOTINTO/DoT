064ddab500441c4f9f50ea64eb16d7ce85c175b3
DONOTINTO <goods99j@gmail.com>
2024-03-08 18:17:02 +0900

    create. Dashboard CollectionView 전용 Compositional Layout 파일 생성

---
+++ DoT/DoT/Model/CompositionalLayout/DashboardCL.swift
@@ -0,0 +1,61 @@
+//
+//  DashboardCompositionalLayout.swift
+//  DoT
+//
+//  Created by 이중엽 on 3/8/24.
+//
+
+import UIKit
+
+enum DashboardCompositionalLayout: Int, CaseIterable {
+    
+    case intro
+    case tripCard
+    case exchangeRate
+    
+    static func create() -> UICollectionViewCompositionalLayout {
+        
+        return UICollectionViewCompositionalLayout { section, environment in
+            
+            let section = DashboardCompositionalLayout(rawValue: section)
+            
+            switch section {
+            case .intro:
+                return section?.createIntro()
+            case .tripCard:
+                return section?.createTripCard()
+            case .exchangeRate:
+                return section?.createExchangeRate()
+            case nil:
+                return nil
+            }
+        }
+    }
+    
+    private func createIntro() -> NSCollectionLayoutSection {
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .fractionalHeight((1.0)))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .estimated(1.0),
+                                               heightDimension: .fractionalHeight(1.0 / 10.0))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20)
+        
+        return section
+    }
+    
+    private func createTripCard() -> NSCollectionLayoutSection? {
+        
+        return nil
+    }
+    
+    private func createExchangeRate() -> NSCollectionLayoutSection? {
+        
+        return nil
+    }
+}
