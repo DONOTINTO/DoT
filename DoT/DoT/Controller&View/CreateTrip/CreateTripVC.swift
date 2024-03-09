ea721dd5f235b3885fe42638ed7fc7adfbcc38e5
DONOTINTO <goods99j@gmail.com>
2024-03-09 12:32:57 +0900

    create. CreateTrip VC/View 파일 생성

---
+++ DoT/DoT/Controller&View/CreateTrip/CreateTripVC.swift
@@ -0,0 +1,16 @@
+//
+//  CreateTripViewController.swift
+//  DoT
+//
+//  Created by 이중엽 on 3/9/24.
+//
+
+import UIKit
+
+class CreateTripViewController: BaseViewController<CreateTripView> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ DoT/DoT/Controller&View/CreateTrip/CreateTripView.swift
@@ -0,0 +1,12 @@
+//
+//  CreateTripView.swift
+//  DoT
+//
+//  Created by 이중엽 on 3/9/24.
+//
+
+import UIKit
+
+class CreateTripView: BaseView {
+
+}
