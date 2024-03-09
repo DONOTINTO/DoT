c7de965d47accd736c1e1b6021dc3df615355329
DONOTINTO <goods99j@gmail.com>
2024-03-09 15:20:18 +0900

    feat. Create Trip UI

--- DoT/DoT/Controller&View/CreateTrip/CreateTripVC.swift
+++ DoT/DoT/Controller&View/CreateTrip/CreateTripVC.swift
@@ -13,4 +13,48 @@ class CreateTripViewController: BaseViewController<CreateTripView> {
         super.viewDidLoad()
 
     }
+    
+    override func configureNavigation() {
+        
+        let symbolImage = UIImage.symbol
+        let symbolBarButtonItem = UIBarButtonItem(image: symbolImage, style: .plain, target: nil, action: nil)
+        symbolBarButtonItem.isEnabled = false
+        
+        navigationItem.leftBarButtonItem = symbolBarButtonItem
+        navigationItem.rightBarButtonItem = makeRightBarButtonItem()
+    }
+    
+    @objc private func createTripButtonClicked() {
+        
+    }
+}
+
+extension CreateTripViewController {
+    
+    private func makeRightBarButtonItem() -> UIBarButtonItem {
+        
+        let title = "새로운 여행 등록하기"
+        
+        var titleAttr = AttributedString.init(title)
+        titleAttr.font = FontManager.getFont(size: .small, scale: .Bold)
+        
+        var buttonConfiguration = UIButton.Configuration.plain()
+        let createTripCustomView = UIButton()
+        
+        let buttonImage = UIImage.plane.withTintColor(.blackWhite, renderingMode: .alwaysOriginal)
+        
+        buttonConfiguration.image = buttonImage
+        buttonConfiguration.imagePadding = 10
+        buttonConfiguration.imagePlacement = .leading
+        buttonConfiguration.title = title
+        buttonConfiguration.attributedTitle = titleAttr
+        buttonConfiguration.baseForegroundColor = .blackWhite
+        buttonConfiguration.baseBackgroundColor = .clear
+        
+        createTripCustomView.configuration = buttonConfiguration
+        createTripCustomView.addTarget(self, action: #selector(createTripButtonClicked), for: .touchUpInside)
+        
+        let createTripBarButton = UIBarButtonItem(customView: createTripCustomView)
+        return createTripBarButton
+    }
 }
--- DoT/DoT/Controller&View/CreateTrip/CreateTripView.swift
+++ DoT/DoT/Controller&View/CreateTrip/CreateTripView.swift
@@ -6,7 +6,178 @@
 //
 
 import UIKit
+import SnapKit
 
 class CreateTripView: BaseView {
 
+    let titleTextField = UITextField()
+    
+    let placeLiteralLabel = UILabel()
+    let placeTextField = UITextField()
+    
+    let headcountLiteralLabel = UILabel()
+    let headcountLabel = UILabel()
+    let headcountStepper = UIStepper()
+    
+    let currencyLiteralLabel = UILabel()
+    let currencyChoiceButton = UIButton()
+    
+    let budgetLiteralLabel = UILabel()
+    let budgetTextField = UITextField()
+    let budgetPerPersonLabel = UILabel()
+    
+    let periodButton = UIButton()
+    
+    override func configureHierarchy() {
+        
+        [titleTextField, placeLiteralLabel, placeTextField, headcountLiteralLabel, headcountLabel, headcountStepper, currencyLiteralLabel, currencyChoiceButton, budgetLiteralLabel, budgetTextField, budgetPerPersonLabel, periodButton].forEach {
+            addSubview($0)
+        }
+    }
+    
+    override func configureLayout() {
+        
+        titleTextField.snp.makeConstraints {
+            $0.top.equalTo(self.safeAreaLayoutGuide).inset(40)
+            $0.horizontalEdges.equalTo(self.safeAreaLayoutGuide).inset(20)
+        }
+        
+        placeLiteralLabel.snp.makeConstraints {
+            $0.top.equalTo(titleTextField.snp.bottom).offset(40)
+            $0.leading.equalTo(self.safeAreaLayoutGuide).inset(20)
+        }
+        
+        placeTextField.snp.makeConstraints {
+            $0.top.equalTo(titleTextField.snp.bottom).offset(40)
+            $0.leading.greaterThanOrEqualTo(placeLiteralLabel.snp.trailing).offset(10)
+            $0.trailing.equalTo(self.safeAreaLayoutGuide).inset(20)
+        }
+        
+        headcountLiteralLabel.snp.makeConstraints {
+            $0.top.equalTo(placeLiteralLabel.snp.bottom).offset(40)
+            $0.leading.equalTo(self.safeAreaLayoutGuide).inset(20)
+        }
+        
+        headcountLabel.snp.makeConstraints {
+            $0.top.equalTo(placeLiteralLabel.snp.bottom).offset(40)
+            $0.leading.greaterThanOrEqualTo(headcountLiteralLabel.snp.trailing).offset(5)
+        }
+        
+        headcountStepper.snp.makeConstraints {
+            $0.centerY.equalTo(headcountLabel.snp.centerY)
+            $0.leading.equalTo(headcountLabel.snp.trailing).offset(10)
+            $0.trailing.equalTo(self.safeAreaLayoutGuide).inset(20)
+        }
+        
+        currencyLiteralLabel.snp.makeConstraints {
+            $0.top.equalTo(headcountLiteralLabel.snp.bottom).offset(40)
+            $0.leading.equalTo(self.safeAreaLayoutGuide).inset(20)
+        }
+        
+        currencyChoiceButton.snp.makeConstraints {
+            $0.top.equalTo(headcountLiteralLabel.snp.bottom).offset(40)
+            $0.trailing.equalTo(self.safeAreaLayoutGuide).inset(20)
+        }
+        
+        budgetLiteralLabel.snp.makeConstraints {
+            $0.top.equalTo(currencyLiteralLabel.snp.bottom).offset(40)
+            $0.leading.equalTo(self.safeAreaLayoutGuide).inset(20)
+        }
+        
+        budgetTextField.snp.makeConstraints {
+            $0.top.equalTo(currencyLiteralLabel.snp.bottom).offset(40)
+            $0.leading.greaterThanOrEqualTo(budgetLiteralLabel.snp.trailing).offset(10)
+            $0.trailing.equalTo(self.safeAreaLayoutGuide).inset(20)
+        }
+        
+        budgetPerPersonLabel.snp.makeConstraints {
+            $0.top.equalTo(budgetTextField.snp.bottom).offset(10)
+            $0.trailing.equalTo(self.safeAreaLayoutGuide).inset(20)
+        }
+        
+        periodButton.snp.makeConstraints {
+            $0.top.equalTo(budgetPerPersonLabel.snp.bottom).offset(40)
+            $0.horizontalEdges.equalTo(self.safeAreaLayoutGuide).inset(20)
+            $0.height.equalTo(44)
+        }
+    }
+    
+    override func configureView() {
+        
+        let titlePlaceHolder = "여행 제목을 입력해주세요"
+        titleTextField.textAlignment = .left
+        titleTextField.placeholder = titlePlaceHolder
+        titleTextField.keyboardType = .default
+        titleTextField.textColor = .blackWhite
+        titleTextField.font = FontManager.getFont(size: .extraLarge, scale: .Medium)
+        titleTextField.attributedPlaceholder = NSAttributedString(string: titlePlaceHolder, attributes: [.foregroundColor: UIColor.justGray])
+        
+        placeLiteralLabel.text = "여행지"
+        placeLiteralLabel.textColor = .blackWhite
+        placeLiteralLabel.font = FontManager.getFont(size: .regular, scale: .Bold)
+        
+        let placePlaceHolder = "여행지를 입력해주세요"
+        placeTextField.textAlignment = .right
+        placeTextField.placeholder = placePlaceHolder
+        placeTextField.keyboardType = .default
+        placeTextField.textColor = .blackWhite
+        placeTextField.font = FontManager.getFont(size: .regular, scale: .Medium)
+        placeTextField.attributedPlaceholder = NSAttributedString(string: placePlaceHolder, attributes: [.foregroundColor: UIColor.justGray])
+        
+        headcountLiteralLabel.text = "인원"
+        headcountLiteralLabel.textColor = .blackWhite
+        headcountLiteralLabel.font = FontManager.getFont(size: .regular, scale: .Bold)
+        
+        headcountLabel.text = "0명"
+        headcountLabel.textColor = .blackWhite
+        headcountLabel.font = FontManager.getFont(size: .regular, scale: .Medium)
+        
+        headcountStepper.maximumValue = 10
+        headcountStepper.minimumValue = 0
+        headcountStepper.backgroundColor = .whiteBlack
+        
+        currencyLiteralLabel.text = "통화"
+        currencyLiteralLabel.textColor = .blackWhite
+        currencyLiteralLabel.font = FontManager.getFont(size: .regular, scale: .Bold)
+        
+        let currencyTitle = "통화 선택"
+        var currencyTitleAttr = AttributedString.init(currencyTitle)
+        currencyTitleAttr.font = FontManager.getFont(size: .regular, scale: .Medium)
+        
+        var currencyButtonConfiguration = UIButton.Configuration.plain()
+        currencyButtonConfiguration.title = currencyTitle
+        currencyButtonConfiguration.attributedTitle = currencyTitleAttr
+        currencyButtonConfiguration.baseForegroundColor = .justWhite
+        currencyButtonConfiguration.background.backgroundColor = .pointBlue
+        currencyButtonConfiguration.titleAlignment = .trailing
+        currencyChoiceButton.configuration = currencyButtonConfiguration
+        
+        budgetLiteralLabel.text = "예산"
+        budgetLiteralLabel.textColor = .blackWhite
+        budgetLiteralLabel.font = FontManager.getFont(size: .regular, scale: .Bold)
+        
+        let budgetPlaceHolder = "0 원"
+        budgetTextField.textAlignment = .right
+        budgetTextField.placeholder = budgetPlaceHolder
+        budgetTextField.keyboardType = .default
+        budgetTextField.textColor = .blackWhite
+        budgetTextField.keyboardType = .decimalPad
+        budgetTextField.font = FontManager.getFont(size: .regular, scale: .Medium)
+        budgetTextField.attributedPlaceholder = NSAttributedString(string: budgetPlaceHolder, attributes: [.foregroundColor: UIColor.justGray])
+        
+        budgetPerPersonLabel.text = "1인당 0원"
+        budgetPerPersonLabel.textColor = .blackWhite
+        budgetPerPersonLabel.font = FontManager.getFont(size: .small, scale: .Medium)
+        
+        let periodTitle = "여행 기간 설정"
+        var periodTitleAttr = AttributedString.init(periodTitle)
+        periodTitleAttr.font = FontManager.getFont(size: .regular, scale: .Bold)
+        
+        var periodButtonConfiguration = UIButton.Configuration.plain()
+        periodButtonConfiguration.title = periodTitle
+        periodButtonConfiguration.attributedTitle = periodTitleAttr
+        periodButtonConfiguration.baseForegroundColor = .justWhite
+        periodButtonConfiguration.background.backgroundColor = .pointBlue
+        periodButton.configuration = periodButtonConfiguration
+    }
 }
