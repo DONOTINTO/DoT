4a3d6d42d49d7052e7bf9a8e91aad4acd3b5f26c
DONOTINTO <goods99j@gmail.com>
2024-03-10 14:10:23 +0900

    clean. width 차이 보정

--- DoT/DoT/Controller&View/CreateTrip/Calendar/CalendarCustomCell.swift
+++ DoT/DoT/Controller&View/CreateTrip/Calendar/CalendarCustomCell.swift
@@ -41,27 +41,29 @@ class CalendarCustomCell: FSCalendarCell {
     
     private func configureLayout() {
         
+        let width = contentView.frame.width.rounded()
+        
         titleLabel.snp.makeConstraints {
             $0.center.equalTo(contentView)
         }
         
         centerCircle.snp.makeConstraints {
             $0.horizontalEdges.equalTo(contentView)
-            $0.height.equalTo(contentView.snp.width)
+            $0.height.equalTo(width)
             $0.centerY.equalTo(contentView)
         }
         
         leftRangeBox.snp.makeConstraints {
             $0.leading.equalTo(contentView)
             $0.trailing.equalTo(contentView.snp.centerX)
-            $0.height.equalTo(contentView.snp.width)
+            $0.height.equalTo(width)
             $0.centerY.equalTo(contentView)
         }
         
         rightRangeBox.snp.makeConstraints {
             $0.trailing.equalTo(contentView)
             $0.leading.equalTo(contentView.snp.centerX)
-            $0.height.equalTo(contentView.snp.width)
+            $0.height.equalTo(width)
             $0.centerY.equalTo(contentView)
         }
     }
@@ -72,9 +74,9 @@ class CalendarCustomCell: FSCalendarCell {
         centerCircle.isHidden = true
         centerCircle.layer.masksToBounds = true
         
-        leftRangeBox.backgroundColor = .justGray.withAlphaComponent(0.3)
+        leftRangeBox.backgroundColor = .justGray
         leftRangeBox.isHidden = true
-        rightRangeBox.backgroundColor = .justGray.withAlphaComponent(0.3)
+        rightRangeBox.backgroundColor = .justGray
         rightRangeBox.isHidden = true
         
         DispatchQueue.main.async {
