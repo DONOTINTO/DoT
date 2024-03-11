04a605e91dbcd2c145d433e7b0137e09b212c782
DONOTINTO <goods99j@gmail.com>
2024-03-11 19:36:05 +0900

    feat. Realm Error 구분

---
+++ DoT/DoT/Model/Realm/RealmError.swift
@@ -0,0 +1,15 @@
+//
+//  RealmError.swift
+//  DoT
+//
+//  Created by 이중엽 on 3/11/24.
+//
+
+import Foundation
+
+enum RealmError: Error {
+    
+    case realmInitializeFailed
+    case objectCreateFailed
+    case objectDeleteFailed
+}
